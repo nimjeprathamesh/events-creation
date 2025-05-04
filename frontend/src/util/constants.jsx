60e1568c38a3ba42474d288cd2aacdea6d6fd154
Prathamesh Nimje <nimjeprathamesh1@gmail.com>
2025-05-04 14:17:14 +0530

    Update constants.jsx

--- frontend/src/util/constants.jsx
+++ frontend/src/util/constants.jsx
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'https://events-creation-backend.vercel.app/';
+export const BACKEND_URL = 'https://events-creation-backend.vercel.app';
 
 export const FRONTEND_URL = 'https://events-creation-app.vercel.app';
